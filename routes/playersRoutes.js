2a226493f404b43dc8a3aa8144292598757eebfb
ArthurMns <arthur.mounissens@gmail.com>
2024-03-27 18:06:21 +0100

    getid / getalll

--- routes/playersRoutes.js
+++ routes/playersRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const playerController = require("../controllers/playersControllers");
 
-// router.get('/', playerController.getAllPlayers);
+router.get('/', playerController.getAllPlayers);
 router.post('/', playerController.createPlayer);
-// router.get("/:id", playerController.getPlayerById);
+router.get("/:id", playerController.getPlayerById);
 
 
 module.exports = router;
--- services/playerServices.js
+++ services/playerServices.js
@@ -4,13 +4,46 @@ async function createPlayer(player) {
     await Player.create(player)
 }
 
+async function getPlayerById(id) {
+    return await Player.findByPk(id);
+}
 
-// async function getPlayerById(id) {
-//   return await Player.findByPk(id);
-// }
+async function getAllPlayers(criterias = {}) {
+    const where = {}
 
-// async function getAllPlayers(criterias = {}) {
+    if (criterias.name) {
+        where.name = criterias.name
+    }
 
-// }
+    if (criterias.age) {
+        where.age = criterias.age
+    }
 
-module.exports = { createPlayer };
+    if (criterias.nationality) {
+        where.nationality = criterias.nationality
+    }
+
+    if (criterias.title) {
+        where.title = criterias.title
+    }
+
+    if (criterias.nbGames) {
+        where.nbGames = criterias.nbGames
+    }
+
+    if (criterias.wins) {
+        where.wins = criterias.wins
+    }
+
+    if (criterias.losses) {
+        where.losses = criterias.losses
+    }
+
+    if (criterias.draws) {
+        where.draws = criterias.draws
+    }
+
+    return await Player.findAll({ where })
+}
+
+module.exports = { createPlayer, getPlayerById, getAllPlayers };
